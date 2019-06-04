516436d526fd99f73028f77fdc94f0cfe05d0434
renan-meneses <renan.meneses@alu.ufc.br>
2019-06-04 19:58:46 -0300

    correçoes

--- conexao.php
+++ conexao.php
@@ -1,24 +1,18 @@
 
-//<?php
+<?php
 
-$host = 'localhost'; //endereco do seu servidor MySQL
-$database = 'PetLivre'; //preencha com o nome do BD que contem a tabela que criamos
-$login_db = 'root'; //login usado para acessar seu BD
-$senha_db = 'devolper'; //senha usada para acessar seu BD
+$host = "127.0.0.1"; //endereco do seu servidor MySQL
+$database = "PetLivre"; //preencha com o nome do BD que contem a tabela que criamos
+$login_db = "web"; //login usado para acessar seu BD
+$senha_db = ""; //senha usada para acessar seu BD
 
 // nao altere mais nada abixo desta linha
 
-$connection = mysql_connect("localhot","root","devolper") 
+$connection = mysqli_connect($host, $login_db, $senha_db, $database) 
     or die ("Nao foi possivel conectar ao servidor.");
     
-$db = mysql_select_db("PetLivre", 'localhost')
-   or die("Nao foi possivel selecionar o banco de dados.");
+//$db = mysqli_select_db($database, $host)
+   //or die("Nao foi possivel selecionar o banco de dados.");
+//$conexao = mysqli_connect($host, $login_db, $senha_db, $database) or die("não conectou"); 
 
-//define('HOST','localhost');
-//define('Usuario', 'root');
-//define('senha', 'devolper');
-//define('DB', 'PetLivre');
-
-//$connection = mysqli_connect(HOST, Usuario, senha, DB) or die("Não foi possivel conectar");
-//$db = mysql_select_db("$database", $connection);
-?> 
+?>
--- login.php
+++ login.php
@@ -12,7 +12,7 @@ $login = $_POST['login'];
 $senha_postada = $_POST['senha'];
 
 
-$sql = mysqli_query($connection, "SELECT * FROM acesso WHERE login='$login' AND senha=md5('$senha_postada')") or die("erro ao selecionar ". mysqli_error());
+$sql = mysqli_query($connection, "SELECT * FROM acesso WHERE login='$login' AND senha=md5('$senha_postada')") or die("erro ao selecionar ");
 
 if( $linha = mysqli_fetch_array($sql)) {
  
@@ -40,8 +40,8 @@ $date1 =date("$h_H:i");
 
 if(date("w") ==0){
 
-// VERIFICA SE USUARIO TEM PERMISSÃO DE ACESSO AOS DOMINGOS
-$sql_config_dias = mysqli_query("SELECT login_dia_semana FROM tab_config_user WHERE id_user='$cod'") or die("erro ao selecionar: tab_config_user");
+// VERIFICA SE USUARIO TEM PERMISSï¿½O DE ACESSO AOS DOMINGOS
+//$sql_config_dias = mysqli_query("SELECT login_dia_semana FROM tab_config_user WHERE id_user='$cod'") or die("erro ao selecionar: tab_config_user");
 
 	if($linha_config_dias = mysqli_fetch_array($sql_config_dias)){
 
@@ -52,31 +52,13 @@ $sql_config_dias = mysqli_query("SELECT login_dia_semana FROM tab_config_user WH
 if(empty($login_dia_semana_db)){
 
 echo '<script>
-alert ("                             Atenção !\n\nAcesso não autorizado!\n\nContate o administrador do sistema.\n\n") 
+alert ("                             Atenï¿½ï¿½o !\n\nAcesso nï¿½o autorizado!\n\nContate o administrador do sistema.\n\n") 
 window.location = "negado.php";
 
 </script>';
 
-}// fecha if(!empty($login_dia_semana_db ...
-
-}// fecha if(date("w") ==0)) ...
-
-// ENVIA SMS QUANDO USUÁRIO SE LOGA NO SITE
-
-// ---  ENVIA SMS  -------
-/*
-$ddd = 11;
-$telefone = 89685158;
-$operadora = 'clarotorpedo.com.br';
-$celular = $ddd.$telefone.'@'.$operadora;
-
-$assunto1 ="Usuario Logado\r\n";
-$mensagem1 = "O usuario: $login, acabou de acessar o sistema as $date1\r\n";
-$header1 = "From: petlivre@maypet.com.br\r\n";
-
-mail($celular, $assunto1, $mensagem1, $header1);
-// ------------------------------------------
-*/
+}
+}
 header("Location: $url_db");
 
 
